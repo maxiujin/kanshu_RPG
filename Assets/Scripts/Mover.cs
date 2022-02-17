d56b684b8e6285d3e705b166519ac9f1ca3d6ddd
Matthew Thompkins <alkeccg@gmail.com>
2022-02-17 01:43:18 -0800

    mouse+player movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,17 +10,33 @@ public class Mover : MonoBehaviour
     [SerializeField] Transform target; 
     Vector3 destination;
     NavMeshAgent agent; 
-    [SerializeField] float speed= 10f; 
+   
 
-    // Start is called before the first frame update
-
-
-    // Update is called once per frame
     void Update()
     {//
-       
-            GetComponent<NavMeshAgent>().destination = target.position; 
+
+    if(Input.GetMouseButtonDown(0))
+    { //if(Input.GetKey(KeyCode.Mouse0)){
         
+       MoveToCursor();
+
+    }
+       
         
     }
+
+    private void MoveToCursor () 
+    {
+     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+     RaycastHit hit; 
+
+     bool hasHit = Physics.Raycast(ray, out hit);
+     if(hasHit) 
+     {
+          GetComponent<NavMeshAgent>().destination = hit.point; 
+     }
+    }
+
+       
+            
 }
