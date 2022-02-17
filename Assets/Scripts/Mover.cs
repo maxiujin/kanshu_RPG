2fa0b553d249f2d21a9134a2b0d52e59c504a7f5
Matthew Thompkins <alkeccg@gmail.com>
2022-02-17 00:33:25 -0800

    first commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target; 
+    Vector3 destination;
+    NavMeshAgent agent; 
+    [SerializeField] float speed= 10f; 
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {//
+       
+            GetComponent<NavMeshAgent>().destination = target.position; 
+        
+        
+    }
+}
