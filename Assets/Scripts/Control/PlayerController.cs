9b5aba1f2a3a2f4271740bff13f9059c951bf0c5
Matthew Thompkins <alkeccg@gmail.com>
2022-02-28 03:01:05 -0800

    added action priority

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -14,11 +14,12 @@ public class PlayerController : MonoBehaviour
     
 void Update ()
         {
-            InteractWithCombat();
-            InteractWithMovement();
+            if (InteractWithCombat()) return; 
+            if (InteractWithMovement()) return;
+         
         }
 
-        private void InteractWithCombat()
+        private bool InteractWithCombat()
         {
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());//returns all of the things it hits
             foreach (RaycastHit hit in hits) 
@@ -29,33 +30,30 @@ void Update ()
              if (Input.GetMouseButtonDown(0))
              {
                  GetComponent<Fighter>().Attack(target); 
+            
              }
+             return true; 
 
             }
+            return false;
            
         }
 
-        private void InteractWithMovement()
+        private bool InteractWithMovement()
         {
-            if (Input.GetMouseButton(0))
-            {
-
-                MoveToCursor();
-
-
-            }
-        }
-
-        private void MoveToCursor () 
-    {
-     Ray ray = GetMouseRay(); 
+            
      RaycastHit hit; 
 
-     bool hasHit = Physics.Raycast(ray, out hit);
+     bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
      if(hasHit)
         {
-            GetComponent<Mover>().MoveTo(hit.point);
+             if (Input.GetMouseButton(0))
+            {
+                    GetComponent<Mover>().MoveTo(hit.point);
+            }
+           return true; 
         }
+        return false; 
     }
 
     private static Ray GetMouseRay()
