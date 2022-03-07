c14ac74322ec848115317797cad142bddbc92f54
Matthew Thompkins <alkeccg@gmail.com>
2022-03-07 02:25:13 -0800

    attack + damage'

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -3,41 +3,71 @@ using UnityEngine;
 using RPG.Movement; 
 using RPG.Core;
 
+
+
 namespace RPG.Combat 
 {
     public class Fighter : MonoBehaviour, IAction
 {
     [SerializeField] float weaponRange = 2f; 
+    [SerializeField] float timeBetweenAttacks =1f;
+    
+    [SerializeField] float weaponDamage = 5f;
+
     Transform target; 
+    float timeSinceLastAttack = 0; //how long has passed since the last attack
 
    private void Update()
     {
+
+        timeSinceLastAttack+=Time.deltaTime; 
         if(target==null)return;
 
         if(!GetIsInRange())
         {
         GetComponent<Mover>().MoveTo(target.position); 
+        
+        
 
         }
         else
-        {
-            GetComponent<Mover>().Cancel();
+            {
+                GetComponent<Mover>().Cancel();
+                AttackBehavior();
+
+            }
 
         }
-        
-    }
 
-    private bool GetIsInRange ()
+        private void AttackBehavior()
+        {
+            //this will trigger the hit() event
+            if(timeSinceLastAttack>timeBetweenAttacks){
+            GetComponent<Animator>().SetTrigger("attack");
+            timeSinceLastAttack = 0;
+
+            }
+           
+           
+        }
+
+        private bool GetIsInRange ()
     {
         return Vector3.Distance(transform.position, target.position) < weaponRange;
     }
     public void Attack(CombatTarget combatTarget) 
     {
-        print("attack now"); 
+        print("attack now"+timeSinceLastAttack); 
+  
+        
 
         GetComponent<ActionScheduler>().StartAction(this); 
 
         target = combatTarget.transform; 
+       
+       
+     
+       
        
     }
 
@@ -46,6 +76,18 @@ namespace RPG.Combat
         
         target=null;
 
+
+    }
+
+
+
+
+//this is an Animation Event
+    void Hit ()
+    {
+        
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage); 
 
     }
 }
