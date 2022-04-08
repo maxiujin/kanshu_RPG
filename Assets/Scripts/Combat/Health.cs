09dfdd6c6da12bfcadfbf6e6020731220654f1cd
matt thompkins <alkeccg@gmail.com>
2022-04-08 02:08:16 -0700

    has faint logic

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -5,24 +5,30 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
-      [SerializeField] float health = 100f;
-      bool hasFainted;
-      
-      public void TakeDamage(float damage)
-      {
+        [SerializeField] float health = 100f;
+        bool hasFainted = false;
 
-        health = Mathf.Max(health-damage,0); 
-        print(health); 
-        if(health==0 && hasFainted)
+        public void TakeDamage(float damage)
         {
-         
-          
-           GetComponent<Animator>().SetTrigger("Faint");
-           hasFainted= false; 
-   SpriteSortPoint 
+
+            health = Mathf.Max(health - damage, 0);
+            print(health);
+            if (health == 0)
+            {
+                StopFaint();
+
+            }
+
         }
-        
-      }
+
+        private void StopFaint()
+        {
+            if (hasFainted) return;
+
+            hasFainted = true;
+            GetComponent<Animator>().SetTrigger("Faint");
+
+        }
     }
 
 }
