4f80ce9d86785bd41d8d72e2203f2684369866c1
matt thompkins <alkeccg@gmail.com>
2022-04-08 01:57:16 -0700

    new bool

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
+
 namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
       [SerializeField] float health = 100f;
+      bool hasFainted;
       
       public void TakeDamage(float damage)
       {
 
         health = Mathf.Max(health-damage,0); 
         print(health); 
+        if(health==0 && hasFainted)
+        {
+         
+          
+           GetComponent<Animator>().SetTrigger("Faint");
+           hasFainted= false; 
+   SpriteSortPoint 
+        }
         
       }
     }
